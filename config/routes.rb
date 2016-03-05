7f5ecf2cf7deaf741947b25d8f2d0832c953a1ab
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-03-05 13:28:13 +0000

    Corrections for DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,2 @@
 Rails.application.routes.draw do
-  get 'movies', to: 'movie#index'
-  get 'movie/:id', to: 'movie#show'
-
-  post 'movie/create'
-  put 'movie/edit/:id', to: 'movie#edit'
-  delete 'movie/destroy/:id', to: 'movie#destroy'
 end
