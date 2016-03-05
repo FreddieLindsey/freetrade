a76272ab42b712dd625b5169ef06e13418daaedb
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-03-05 22:47:28 +0000

    Fixed no items in search, user edit/delete

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,8 +7,19 @@ class UserController < ApplicationController
   end
 
   def edit
+    u = User.find_or_create_by(facebook_id: params[:facebook_id])
+    u['name'] = params[:name] if params[:name]
+    u.save if u.valid?
+    render  json: { user: u },
+            status: :ok,
+            content_type: 'text/json'
   end
 
   def delete
+    u = User.find_by(facebook_id: params[:facebook_id])
+    destroyed = u.destroy unless u.nil?
+    render  json: { user: destroyed },
+            status: :ok,
+            content_type: 'text/json'
   end
 end
