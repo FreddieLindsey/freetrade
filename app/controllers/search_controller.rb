e5a279f60ca6e1f4951afca2ead0aa4f3f7c6212
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-03-06 00:58:07 +0000

    Added images endpoint and url returning for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 require 'active_support'
 
 class SearchController < ApplicationController
+  @@awspaa = Vacuum.new 'GB'
+  @@awspaa.configure(
+    aws_access_key_id: ENV['AWS_ACCESS_ID'],
+    aws_secret_access_key: ENV['AWS_ACCESS_SECRET'],
+    associate_tag: 'tag'
+  )
+
   def indices
     render  json: { indices: [ 'All', 'Apparel', 'Automotive', 'Baby', 'Beauty', 'Blended', 'Books', 'Classical', 'DigitalMusic', 'DVD', 'Electronics', 'ForeignBooks', 'GourmetFood', 'Grocery', 'HealthPersonalCare', 'Hobbies', 'HomeGarden', 'HomeImprovement', 'Industrial', 'Jewelry', 'KindleStore', 'Kitchen', 'Lighting', 'Magazines', 'Merchants', 'Miscellaneous', 'MP3Downloads', 'Music', 'MusicalInstruments', 'MusicTracks', 'OfficeProducts', 'OutdoorLiving', 'Outlet', 'PCHardware', 'PetSupplies', 'Photo', 'Shoes', 'SilverMerchants', 'Software', 'SoftwareVideoGames', 'SportingGoods', 'Tools', 'Toys', 'UnboxVideo', 'VHS', 'Video', 'VideoGames', 'Watches', 'Wireless' 'WirelessAccessories' ] },
             status: :ok,
@@ -15,19 +22,12 @@ class SearchController < ApplicationController
       return
     end
 
-    awspaa = Vacuum.new 'GB'
-    awspaa.configure(
-      aws_access_key_id: ENV['AWS_ACCESS_ID'],
-      aws_secret_access_key: ENV['AWS_ACCESS_SECRET'],
-      associate_tag: 'tag'
-    )
-
     query = {
       'Keywords' => "#{params[:query]}",
       'SearchIndex' => "#{params[:index]}"
     }
 
-    response = awspaa.item_search( query: query )
+    response = @@awspaa.item_search( query: query )
     response = response.to_h
     items = response['ItemSearchResponse']['Items']['Item']
 
@@ -36,12 +36,36 @@ class SearchController < ApplicationController
       items.each do |i|
         item = {}
         item['ASIN'] = i['ASIN']
+        item['url'] = i['DetailPageURL']
         item['title'] = i['ItemAttributes']['Title']
         results.push(item)
       end
     end
 
     render  json: { items: results },
+            status: :ok,
+            content_type: 'text/json'
+  end
+
+  def images
+    if !params[:query]
+      render  json: {},
+              status: :bad_request,
+              content_type: 'text/json'
+      return
+    end
+
+    query = {
+      'Keywords' => "#{params[:query]}",
+      'SearchIndex' => "#{params[:index]}",
+      'ResourceGroup' => 'Images'
+    }
+
+    response = @@awspaa.item_search( query: query )
+    response = response.to_h
+    items = response['ItemSearchResponse']['Items']['Item']
+
+    render  json: { items: items },
             status: :ok,
             content_type: 'text/json'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'api/searchopts', to: 'search#indices'
   post 'api/search', to: 'search#search'
+  post 'api/search/images', to: 'search#images'
 
   post 'api/products', to: 'products#index'
   post 'api/products/add'
