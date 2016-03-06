77a37f94f8b9a0edcbbd6dcaf214da70ef2e8eeb
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-03-06 02:36:33 +0000

    Add a product association on adding a product to ones wishlist

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,19 +16,41 @@ class ProductsController < ApplicationController
   def add
     u = get_user params[:facebook_id]
     unless u.nil?
-      product = Product.new(name: params[:name], user_id: u.id)
-      unless product.valid?
-        render  json: { message: 'Product has invalid name', product_error: product.errors.full_messages },
-                status: :bad_request,
-                content_type: 'text/json'
-        return
-      else
-        product.save
-        render  json: { product: product },
-                status: 200,
-                content_type: 'text/json'
-        return
+      product = Product.find_by(
+        asin: params[:asin],
+        name: params[:name],
+        url: params[:url]
+      )
+      if product.nil?
+        product = Product.new(
+          asin: params[:asin],
+          name: params[:name],
+          url: params[:url]
+        )
+        unless product.valid?
+          render  json: {
+                    message: 'Product has invalid parameters',
+                    product_error: product.errors.full_messages
+                  },
+                  status: :bad_request,
+                  content_type: 'text/json'
+          return
+        else
+          product.save
+        end
       end
+      pa = ProductAssociation.find_or_create_by(
+        user_id: u.id,
+        product_id: product.id,
+        complete: false,
+        start_date: params[:start_date],
+        end_date: params[:end_date],
+        probability: params[:probability]
+      )
+      render  json: { product: product, association: pa },
+              status: 200,
+              content_type: 'text/json'
+      return
     else
       render  json: { message: "No user found: id #{params[:facebook_id]}" },
               status: :bad_request,
